6c4430171928130744c0ba604229bdf2695fbf28
ramacosta20 <ramiro20andres@gmail.com>
2017-10-31 10:39:08 -0300

    Word2 a la mitad

--- app/src/main/java/com/example/a42100095/trabajopractico8/clsJuego.java
+++ app/src/main/java/com/example/a42100095/trabajopractico8/clsJuego.java
@@ -2,7 +2,9 @@ package com.example.a42100095.trabajopractico8;
 
 import android.util.Log;
 
+import org.cocos2d.layers.Layer;
 import org.cocos2d.nodes.Director;
+import org.cocos2d.nodes.Scene;
 import org.cocos2d.opengl.CCGLSurfaceView;
 
 /**
@@ -20,5 +22,36 @@ public class clsJuego {
     {
         Log.d("Comenzar","Comienza el juego");
         Director.sharedDirector().attachInView(_VistaDelJuego);
+    }
+    private Escena()
+    {
+        Log.d("EscenaDelJuego","Comienza el armado de la escena del juego");
+
+        Log.d("EscenaDelJuego","Declaro e instancio la escena en si");
+        Scene EscenaADevolver;
+        EscenaADevolver= Scene.node();
+
+        Log.d("EscenaDelJuego","Declaro e instancio la capa que va a contener la imagen de fondo");
+        CapaDeFondo MiCapaDeFondo;
+        MiCapaDeFondo = new CapaDeFondo();
+
+        Log.d("EscenaDelJuego","Declaro e instancio la capa que va a contener el jugador y los enemigos");
+        CapaDeFrente MiCapaDeFrente;
+        MiCapaDeFrente = new CapaDeFrente();
+
+        Log.d("EscenaDelJuego","Agrego a la escena la capa del fondo mas atras, y la del frente mas adelante");
+        EscenaADevolver.addChild(MiCapaDeFondo, -10);
+        EscenaADevolver.addChild(MiCapaDeFrente, 10);
+
+        Log.d("EscenaDelJuego","Devuelvo la escena ya armada");
+        return EscenaADevolver;
+
+
+    }
+    class CapaDeFondo extends Layer {
+
+    }
+    class CapaDeFrente extends Layer {
+
     }
 }
